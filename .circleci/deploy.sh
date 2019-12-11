cbba6f7abe9354a338c312f446d9a39df997f05a
Nicolas Dupont <npg.dupont@gmail.com>
2019-12-11 15:02:50 +0100

    Use CI to build and deploy site to GitHub pages

---
+++ .circleci/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+REPO="https://${GITHUB_BOT_TOKEN}@github.com/ambanum/pariscall.international.git"
+
+git checkout gh-pages
+git pull origin gh-pages
+
+find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv _site/* .
+rm -R _site/
+
+git add -fA
+git commit --allow-empty -m "$(git log master -1 --pretty=%B)
+[skip ci]"
+git push $REPO gh-pages
+
+echo "Deployed successfully"
